d596e7e2b31ae5032d5f6e5ec43e1d2aaca68739
fabianbohn <Mystek.fb@googlemail.com>
2022-05-18 12:55:16 +0200

    Change 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+
 
 function Form(props) {
+
+  const [name, setName] = useState('');
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
+
+
+
   return (
-    <form>
-      <h2 className="label-wrapper">
+    <form onSubmit={handleSubmit}>
+
+    <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
         </label>
@@ -14,6 +33,8 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const DATA = [
   { id: "todo-2", name: "Repeat", completed: false }
 ];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
+    <App tasks={DATA} />
   </React.StrictMode>
 );
 
